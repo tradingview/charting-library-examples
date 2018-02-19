6deca6cf0b29cd7d356fed162d99c3665c1b561d
Eugene Timokhov <timocov@gmail.com>
2018-02-19 13:27:07 +0300

    Simplify react example

--- react-example/src/components/TVChartContainer/index.tsx
+++ react-example/src/components/TVChartContainer/index.tsx
@@ -2,83 +2,43 @@ import * as React from 'react';
 import './index.css';
 import {
 	ChartingLibraryWidgetOptions,
-	IBasicDataFeed,
-	IDatafeedQuotesApi,
-	ResolutionString,
-	AccessList,
 	LanguageCode,
-	NumericFormattingParams,
-	Timezone,
-	AvailableSaveloadVersions,
-	StudyOverrides,
-	CustomFormatters,
-	Overrides,
-	Favorites,
-	IExternalSaveLoadAdapter,
-	LoadingScreenOptions,
-	ISettingsAdapter
 } from '../../../public/charting_library/charting_library.min';
 
 export interface ChartContainerProps {
-	symbol?: string;
-	interval?: ResolutionString;
-	containerId?: string;
-	datafeed?: IBasicDataFeed | (IBasicDataFeed & IDatafeedQuotesApi);
-	datafeedUrl?: string;
-	autoSaveDelay?: number;
-	autosize?: boolean;
-	debug?: boolean;
-	disabledFeatures?: string[];
-	drawingsAccess?: AccessList;
-	enabledFeatures?: string[];
-	fullscreen?: boolean;
-	height?: number;
-	libraryPath?: string;
-	locale?: LanguageCode;
-	numericFormatting?: NumericFormattingParams;
-	savedData?: object;
-	studiesAccess?: AccessList;
-	studyCountLimit?: number;
-	symbolSearchRequestDelay?: number;
-	timezone?: 'exchange' | Timezone;
-	toolbarBg?: string;
-	width?: number;
-	chartsStorageUrl?: string;
-	chartsStorageApiVersion?: AvailableSaveloadVersions;
-	clientId?: string;
-	userId?: string;
-	loadLastChart?: boolean;
-	studiesOverrides?: StudyOverrides;
-	customFormatters?: CustomFormatters;
-	overrides?: Overrides;
-	snapshotUrl?: string;
-	indicatorsFileName?: string;
-	preset?: 'mobile';
-	customCssUrl?: string;
-	favorites?: Favorites;
-	saveLoadAdapter?: IExternalSaveLoadAdapter;
-	loadingScreen?: LoadingScreenOptions;
-	settingsAdapter?: ISettingsAdapter;
+	symbol: ChartingLibraryWidgetOptions['symbol'];
+	interval: ChartingLibraryWidgetOptions['interval'];
+
+	// BEWARE: no trailing slash is expected in feed URL
+	datafeedUrl: string;
+	libraryPath: ChartingLibraryWidgetOptions['library_path'];
+	chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url'];
+	chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version'];
+	clientId: ChartingLibraryWidgetOptions['client_id'];
+	userId: ChartingLibraryWidgetOptions['user_id'];
+	fullscreen: ChartingLibraryWidgetOptions['fullscreen'];
+	autosize: ChartingLibraryWidgetOptions['autosize'];
+	studiesOverrides: ChartingLibraryWidgetOptions['studies_overrides'];
+	containerId: ChartingLibraryWidgetOptions['container_id'];
 }
 
 export interface ChartContainerState {
 }
 
-function getParameterByName(name: string) {
-	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-	const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+function getLanguageFromURL(): LanguageCode | null {
+	const regex = new RegExp('[\\?&]lang=([^&#]*)');
 	const results = regex.exec(location.search);
-	return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode;
 }
 
-export class TVChartContainer extends React.PureComponent<ChartContainerProps, ChartContainerState> {
+export class TVChartContainer extends React.PureComponent<Partial<ChartContainerProps>, ChartContainerState> {
 	public static defaultProps: ChartContainerProps = {
 		symbol: 'AAPL',
 		interval: 'D',
 		containerId: 'tv_chart_container',
 		datafeedUrl: 'https://demo_feed.tradingview.com',
 		libraryPath: 'charting_library/',
-		chartsStorageUrl: 'http://saveload.tradingview.com',
+		chartsStorageUrl: 'https://saveload.tradingview.com',
 		chartsStorageApiVersion: '1.1',
 		clientId: 'tradingview.com',
 		userId: 'public_user_id',
@@ -87,22 +47,17 @@ export class TVChartContainer extends React.PureComponent<ChartContainerProps, C
 		studiesOverrides: {},
 	};
 
-	public constructor(props: ChartContainerProps) {
-		super(props);
-	}
-
 	public componentDidMount(): void {
 		const widgetOptions: ChartingLibraryWidgetOptions = {
 			symbol: this.props.symbol as string,
-			interval: this.props.interval as ResolutionString,
-			container_id: this.props.containerId as string,
 			// BEWARE: no trailing slash is expected in feed URL
 			// tslint:disable-next-line:no-any
 			datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(this.props.datafeedUrl),
-			library_path: this.props.libraryPath,
-			locale: (getParameterByName('lang') || 'en') as LanguageCode,
-			// Regression Trend-related functionality is not implemented yet, so it's hidden for a while
-			drawings_access: { type: 'black', tools: [ { name: 'Regression Trend' } ] },
+			interval: this.props.interval as ChartingLibraryWidgetOptions['interval'],
+			container_id: this.props.containerId as ChartingLibraryWidgetOptions['container_id'],
+			library_path: this.props.libraryPath as string,
+
+			locale: getLanguageFromURL() || 'en',
 			disabled_features: ['use_localstorage_for_settings'],
 			enabled_features: ['study_templates'],
 			charts_storage_url: this.props.chartsStorageUrl,
@@ -111,7 +66,7 @@ export class TVChartContainer extends React.PureComponent<ChartContainerProps, C
 			user_id: this.props.userId,
 			fullscreen: this.props.fullscreen,
 			autosize: this.props.autosize,
-			studies_overrides: this.props.studiesOverrides as StudyOverrides,
+			studies_overrides: this.props.studiesOverrides,
 		};
 
 		// tslint:disable-next-line:no-any
@@ -120,13 +75,18 @@ export class TVChartContainer extends React.PureComponent<ChartContainerProps, C
 			const widget = (window as any).tvWidget = new (window as any).TradingView.widget(widgetOptions);
 
 			widget.onChartReady(() => {
-				widget.createButton()
+				const button = widget.createButton()
 					.attr('title', 'Click to show a notification popup')
 					.addClass('apply-common-tooltip')
 					.on('click', () => widget.showNoticeDialog({
 						title: 'Notification',
 						body: 'TradingView Charting Library API works correctly',
-					}))[0].innerHTML = 'Check API';
+						callback: () => {
+							console.log('Noticed!');
+						},
+					}));
+
+				button[0].innerHTML = 'Check API';
 			});
 		});
 	}
