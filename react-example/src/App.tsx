8c4b3b2a1b0913c49762c7849bcb46f2dccef1b7
Eugene Timokhov <timocov@gmail.com>
2018-02-19 10:57:00 +0300

    Removed trailing whitespace

--- react-example/src/App.tsx
+++ react-example/src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className={ 'App' }>
-				<header className={ 'App-header' }>					
+				<header className={ 'App-header' }>
 					<h1 className={ 'App-title' }>
 						TradingView Charting Library and React Integration Example
 					</h1>
