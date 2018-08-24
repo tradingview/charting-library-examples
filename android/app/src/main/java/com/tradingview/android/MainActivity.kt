e82166794d782bdd217f46ae469d1e3f18b32cf5
Eugene Timokhov <timocov@gmail.com>
2018-08-24 15:54:24 +0300

    [android] Show toast when interval is changed

--- android/app/src/main/java/com/tradingview/android/MainActivity.kt
+++ android/app/src/main/java/com/tradingview/android/MainActivity.kt
@@ -16,9 +16,42 @@ class MainActivity : AppCompatActivity() {
         val webView = WebView(this)
         webView.settings.javaScriptEnabled = true
         webView.settings.allowFileAccessFromFileURLs = true
+
+        // to prevent scaling text
+        // see https://github.com/tradingview/charting_library/issues/3267#issuecomment-415031298
         webView.settings.textZoom = 100
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("file:///android_asset/charting_library/index.html")
+
+        val chartingLibraryUrl = "file:///android_asset/charting_library/index.html"
+        val jsBridge = JSApplicationBridge(this)
+        webView.addJavascriptInterface(jsBridge, "ApplicationBridge")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+                if (!url.equals(chartingLibraryUrl)) {
+                    return
+                }
+
+                webView.evaluateJavascript("""
+                    tvWidget.onChartReady(function() {
+                        tvWidget.chart().onIntervalChanged().subscribe(
+                            null,
+                            function(interval) {
+                                ApplicationBridge.onIntervalChanged(interval);
+                            }
+                        );
+                    });
+                """) {
+                    // do nothing
+                }
+            }
+        }
+
+        // uncomment next line if you want to debug WebView in Chrome DevTools
+        // WebView.setWebContentsDebuggingEnabled(true)
+
+        webView.loadUrl(chartingLibraryUrl)
         setContentView(webView)
     }
 }
