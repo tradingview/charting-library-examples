d333da1c87776d3b9bbfc5995714798a003b9247
Eugene Timokhov <timocov@gmail.com>
2018-08-24 13:29:20 +0300

    [android] Removed activity xml file and create webview in code

--- android/app/src/main/java/com/tradingview/android/MainActivity.kt
+++ android/app/src/main/java/com/tradingview/android/MainActivity.kt
@@ -2,21 +2,23 @@ package com.tradingview.android
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
 import android.webkit.WebViewClient
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        bindChart()
+        initWebView()
     }
 
-    private fun bindChart() {
+    private fun initWebView() {
+        val webView = WebView(this)
         webView.settings.javaScriptEnabled = true
         webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.textZoom = 100
         webView.webViewClient = WebViewClient()
         webView.loadUrl("file:///android_asset/charting_library/mobile_white.html")
+        setContentView(webView)
     }
 }
