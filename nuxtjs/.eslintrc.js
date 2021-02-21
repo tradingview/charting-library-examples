c4c3352fd998c3791b9ae5bfe306d4ef553a3d4a
vlad.legusha <vlad.legusha@gmail.com>
2021-02-21 14:00:58 +0200

    [nuxtjs] Implemented example with TVChart

---
+++ nuxtjs/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
+}
---
+++ nuxtjs/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <TVChartContainer/>
+</template>
