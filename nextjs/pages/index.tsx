2674a040af76f424c5880949f3bfc3dd33fc14cd
a.khodaiedoust <a.khodaiedoust@emofid.com>
2023-05-05 13:36:20 +0330

    fixed: it's better to use "onReady" instead of "onLoad" to work on pages switch.

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <Script
         src="/static/datafeeds/udf/dist/bundle.js"
         strategy="lazyOnload"
-        onLoad={() => {
+        onReady={() => {
           setIsScriptReady(true);
         }}
       />
