fcbde83417ffe609ad9e4bb5e97b5dacca31f36e
Razz19 <ehsan8643@gmail.com>
2023-08-05 20:32:53 +0300

    Charting Library with Sveltekit
    example for Sveltekit and Svelte

---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import TvChartContainer from "../components/TVChartContainer.svelte";
+  import { version } from "$lib/charting_library";
+</script>
+
+<p>Library version: {version()}</p>
+
+<TvChartContainer />
+
+<style>
+  p {
+    color: rgb(124, 89, 7);
+    font-family: "Comic Sans MS", cursive;
+    font-size: 0.8em;
+  }
+</style>
